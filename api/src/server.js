cfcc0af88dd3e942c0635d0ff3350cbba48163fa
Alexander Quesada <alexander.quesadaquesada@ucr.ac.cr>
2025-04-18 13:44:52 -0600

    fix: Remove unused MongoDB connection function and clean up CORS middleware

--- api/src/server.js
+++ api/src/server.js
@@ -2,16 +2,12 @@ import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
 import dotenv from 'dotenv';
-import connectDB from './config/db';
 import spaceRoutes from './routes/spaceRoutes';
 import hourlyDataRoutes from './routes/hourlyDataRoutes';
 import reservationRoutes from './routes/reservationRoutes';
 import mongoose from 'mongoose';
 
 dotenv.config();
-
-// Connect to MongoDB
-connectDB();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -21,9 +17,6 @@ const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/smart-
 app.use(cors());
 app.use(helmet());
 app.use(express.json());
-app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:3000'
-}));
 
 // Routes
 app.use('/api/spaces', spaceRoutes);
@@ -37,7 +30,6 @@ app.get('/', (req, res) => {
 
 mongoose.connect(MONGODB_URI).then(() => console.log('MongoDB connected'))
 .catch(err => console.error('MongoDB connection error:', err));
-
 
 // Start server
 app.listen(PORT, () => {
